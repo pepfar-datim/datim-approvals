611e6a4777221d80e8d0ad7d0536c12482b2630d
Mark Polak <mark@thedutchies.com>
2014-10-23 16:01:54 +0200

    Fix small style error

--- src/main/app.js
+++ src/main/app.js
@@ -484,7 +484,7 @@ function viewTableViewController($scope, $controller) {
     }).bind(this));
 }
 
-angular.module('PEPFAR.approvals', ['d2', 'd2-translate', 'ui.select', 'ui.bootstrap.tabs', 'd2-typeahead', 'ui.bootstrap.typeahead', 'ui.bootstrap.progressbar']);
+angular.module('PEPFAR.approvals', ['d2', 'd2-translate', 'ui.select', 'ui.bootstrap.tabs', 'd2-typeahead', 'ui.bootstrap.typeahead', 'ui.bootstrap.progressbar', 'd2Menu']);
 angular.module('PEPFAR.approvals').controller('appController', appController);
 angular.module('PEPFAR.approvals').controller('tableViewController', tableViewController);
 angular.module('PEPFAR.approvals').controller('acceptTableViewController', acceptTableViewController);
--- src/test/mocks/d2_mock.js
+++ src/test/mocks/d2_mock.js
@@ -3,3 +3,5 @@ angular.module('d2-translate', []);
 angular.module('d2', []);
 
 angular.module('d2-translate').service('$translate', function () {});
+
+angular.module('d2Menu', []);
