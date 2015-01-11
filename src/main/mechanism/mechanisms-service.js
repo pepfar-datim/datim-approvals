10cbe2d6bb2fbce2f2376761561fe89e716f7e77
Mark Polak <mark@thedutchies.com>
2015-01-11 17:27:13 +0100

    Fix bug for return/recall display

--- src/main/mechanism/mechanisms-service.js
+++ src/main/mechanism/mechanisms-service.js
@@ -240,7 +240,7 @@ function mechanismsService(d2Api, $log, $q, approvalLevelsService) {
             }
             if (mechanismStatus.permissions.mayUnapprove === true) {
                 mechanism.mayUnapprove = true;
-                if (mechanism.mayApprove) {
+                if (mechanism.mayAccept) {
                     actions.push('Return submission');
                 } else {
                     actions.push('Recall submission');
