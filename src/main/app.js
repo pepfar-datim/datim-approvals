2e1a58366c2c4234451666f3c09030eb0b7b03e3
Thomas Zemp <tzemp@baosystems.com>
2019-01-08 16:30:24 +0100

    #6 make all datasets display if there is no dataset property for period

--- src/main/app.js
+++ src/main/app.js
@@ -546,7 +546,7 @@ function appController(periodService, $scope, currentUser, mechanismsService,
         .subscribe(function (period) {
             // $log.info('Period changed to',  period);
             $scope.details.period = period.id;
-            $scope.details.validDataSets = period.datasets; //TOM (2018-01-08)(for Global ticket #3332)
+            if(period.hasOwnProperty('datasets')){$scope.details.validDataSets = period.datasets}else{$scope.details.validDataSets=[]} //TOM (2018-01-08)(for Global ticket #3332)
             mechanismsService.period = $scope.details.period;
 
             //TODO: See if we can resolve this a bit more clever (It's duplicate with other stuff)
