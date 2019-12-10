0928b9a5e3971394e12ab3e9a360e5c882168309
Jakub Flaska <jflaska@baosystems.com>
2019-12-10 13:09:23 -0500

    test working

---
+++ cypress/integration/formRender/aggregateData.test.js
@@ -0,0 +1,8 @@
+describe('Aggregate Data', ()=> {
+    it('login', () => {
+        cy.loginAs('approvals-interAgency-zambia');
+    });
+    it('Should see data for 17514', ()=>{
+
+    });
+});
---
+++ cypress/integration/multipleMechanisms/multipleMechanismOverview.test.js
@@ -0,0 +1,13 @@
+describe('Multiple Mechanisms Overview', ()=> {
+    it('login', () => {
+        cy.loginAs('approvals-interAgency-zambia');
+    });
+    it('Should see Mechanism Overview', ()=>{
+        cy.visit('/#/action?action=view&approvalCombos=f5RoebaDLMx%3ABOKwiEdAMzQ%3AJPmbX9ZfXmd%3A&approvalCombos=f5RoebaDLMx%3ABaAWvLUBS9a%3Ao9IHCJEfiXL%3A&period=2019Q3&workflow=RwNpkAM7Hw7');
+        cy.containsAll([
+            'All Mechanisms Overview',
+            'USAID, HHS/CDC',
+            'RIGHT TO CARE, JHPIEGO CORPORATION',
+        ]);
+    });
+});
