a9e6fee61ac4e5515436f486fc3733d8dd5c3f3d
Jakub Flaska <jflaska@baosystems.com>
2019-12-13 13:15:31 -0500

    upgrades, fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from 'react-dom';
-import {baseUrl, serverUrl} from "./modules/shared/services/apiUrl.service";
+import {baseUrl} from "./modules/shared/services/apiUrl.service";
 import {init, config} from 'd2';
 import {HeaderBar} from '@dhis2/ui-widgets'
 import {Provider} from '@dhis2/app-runtime'
@@ -9,12 +9,12 @@ import ThemeWrapper from "./modules/main/components/themeWrapper.component";
 import NetworkError from "./modules/main/components/networkError.component";
 import "./index.css";
 
-config.baseUrl = baseUrl;
+config.baseUrl = baseUrl + 'api';
 config.i18n.sources.add('i18n.txt');
 
 function Dhis2(){
     return (
-        <Provider config={{baseUrl: serverUrl, apiVersion: '30'}}>
+        <Provider config={{baseUrl: baseUrl, apiVersion: '30'}}>
             <span id='dhis2HeaderBar'>
                 <HeaderBar/>
             </span>
