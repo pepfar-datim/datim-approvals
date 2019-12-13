4bd6e634f39f635347ea8d5f5c62fc16abe96f1a
Jakub Flaska <jflaska@baosystems.com>
2019-12-13 13:27:08 -0500

    all working ok

--- cypress/support/login/login.js
+++ cypress/support/login/login.js
@@ -1,15 +1,15 @@
-const serverUrl = require('../../../src/config/serverConfig.dev').cy_serverUrl;
+const baseUrl = require('../../../src/config/serverConfig.dev').baseUrl;
 const userPassword = require('../../../src/config/testConfig.dev').userPassword;
 
 Cypress.Commands.add('loginAs', (userType)=>{
     cy.request({
         method: 'POST', 
-        url: `${serverUrl}dhis-web-commons-security/login.action`,
+        url: `${baseUrl}dhis-web-commons-security/login.action`,
         body: `j_username=cypress-${userType}&j_password=${userPassword}`,
         headers: {
-            'authority': serverUrl,
-            'origin': serverUrl,
-            'referer': `${serverUrl}dhis-web-commons-security/login.action`,
+            'authority': baseUrl,
+            'origin': baseUrl,
+            'referer': `${baseUrl}dhis-web-commons-security/login.action`,
             'content-type': 'application/x-www-form-urlencoded',
         }
     }).then((response)=>{ 
