7b2163642a90b2dfb2e626d6638deba3178dcbee
Jakub Flaska <jflaska@baosystems.com>
2019-12-16 14:50:45 -0500

    test fix

--- cypress/integration/formRender/aggregateData.test.js
+++ cypress/integration/formRender/aggregateData.test.js
@@ -12,7 +12,7 @@ describe('Aggregate Data', ()=> {
         cy.loginAs('superAdmin');
     });
     it('Should see All Mechanisms Overview', ()=>{
-        cy.visit('/#/action?action=view&approvalCombos=f5RoebaDLMx%3ABOKwiEdAMzQ%3AJPmbX9ZfXmd%3A&approvalCombos=f5RoebaDLMx%3ABaAWvLUBS9a%3Ao9IHCJEfiXL%3A&period=2019Q3&workflow=RwNpkAM7Hw7');
+        cy.visit('/#/action?approvalCombos=f5RoebaDLMx%3ABOKwiEdAMzQ%3AJPmbX9ZfXmd%3A&approvalCombos=f5RoebaDLMx%3ABaAWvLUBS9a%3Ao9IHCJEfiXL%3A&period=2019Q3&workflow=RwNpkAM7Hw7');
         cy.containsAll([
             'All Mechanisms Overview',
             'HHS/CDC, USAID',
