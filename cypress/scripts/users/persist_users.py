39c1dc866d3095871039de07f9cf399b196c4f7a
Jakub <jflaska@baosystems.com>
2020-01-29 14:42:19 -0500

    fixed scripts

--- cypress/scripts/users/persist_users.py
+++ cypress/scripts/users/persist_users.py
@@ -1,12 +1,17 @@
 import api
 import json
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-s',  default='users.json', help='source file with users as JSON')
+args = parser.parse_args()
 
 if not os.environ['CYPRESS_TEST_PASSWORD']:
     raise Exception('DHIS2 password for test accounts not set')
 
 def load_users():
-    file = open("users.json", "r")
+    file = open(args.source, "r")
     contents = file.read()
     return json.loads(contents)
 
---
+++ cypress/scripts/users/updateUsers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+userGroupUpdates=('dC7jwUgo23e->y4kPXXeNzxe' 'aaJqXrV1NvU->dBW0Xm1YBkE' '')
+
+for i in ${array[@]}; do
+	echo $i
+done
