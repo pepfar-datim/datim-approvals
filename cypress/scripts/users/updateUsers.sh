62e072eec4ed1714f5bdd167b12395d0fc96e290
Jakub <jflaska@baosystems.com>
2020-01-30 12:22:46 -0500

    all users imported to new release

--- cypress/scripts/users/updateUsers.sh
+++ cypress/scripts/users/updateUsers.sh
@@ -74,7 +74,39 @@
 # "id": "DSBFkL4uzd2",
 # "displayName": "OU Asia Region MOH Users"
 
-userGroupUpdates=('dC7jwUgo23e->y4kPXXeNzxe' 'aaJqXrV1NvU->dBW0Xm1YBkE' 'ZiK5R9d001L->Z67RKj0UUx6' 'f6wtRWtCqZ6->nArTNoncIZZ' 'tVzVUOed3zF->kCQ2gMvkkUg' 'bceRMHIksMJ->rkd3Ok4r0oe' 'm1lYfdanqRQ->vTUZSYiSom4' 'hzDDL5XnD3f->rutVB5w25IP' 'ZH20mYbNuxG->DSBFkL4uzd2')
+# old
+# "id": "FmY9Kli6XOv",
+# "displayName": "OU Cambodia MOH User administrators",
+
+#new
+# "name": "OU Asia Region MOH User administrators",
+# "id": "rkd3Ok4r0oe"
+
+# old
+# "id": "jq83EI17WdN",
+# "displayName": "OU Cambodia All mechanisms",
+
+# new
+# "name": "OU Asia Region All mechanisms",
+# "id": "Z67RKj0UUx6"
+
+# old
+# "id": "bM1CccehzoJ",
+# "displayName": "OU Cambodia User administrators",
+
+# new
+# "name": "OU Asia Region User administrators",
+# "id": "xXvf3vvSaq6"
+
+# old
+# "id": "hnqbnG1nWRe",
+# "displayName": "OU Cambodia Country team",
+
+# new
+# "name": "OU Asia Region Country team",
+# "id": "nArTNoncIZZ"
+
+userGroupUpdates=('dC7jwUgo23e->y4kPXXeNzxe' 'aaJqXrV1NvU->dBW0Xm1YBkE' 'ZiK5R9d001L->Z67RKj0UUx6' 'f6wtRWtCqZ6->nArTNoncIZZ' 'tVzVUOed3zF->kCQ2gMvkkUg' 'bceRMHIksMJ->rkd3Ok4r0oe' 'm1lYfdanqRQ->vTUZSYiSom4' 'hzDDL5XnD3f->rutVB5w25IP' 'ZH20mYbNuxG->DSBFkL4uzd2' 'FmY9Kli6XOv->rkd3Ok4r0oe' 'jq83EI17WdN->Z67RKj0UUx6' 'bM1CccehzoJ->xXvf3vvSaq6' 'hnqbnG1nWRe->nArTNoncIZZ')
 
 replacementRule=''
 
