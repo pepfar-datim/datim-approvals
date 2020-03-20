31186851bf19a8ebfb9c2f86f2f77e05b5ae5ce0
Jakub <jflaska@baosystems.com>
2020-03-20 13:22:28 -0400

    loads all data

--- cypress/scripts/dataEntry/getData.sql
+++ cypress/scripts/dataEntry/getData.sql
@@ -1,9 +1,13 @@
 -- gets data for aggregateData.test.js
-
 SELECT *
 FROM analytics
 WHERE pe='2019Q3'
 AND dx='RhkU5KjtAi6'
 AND (co='xjIOzgKKqaE' OR co='srbCCscTJaK' OR co='nsATUhYrzYh')
---AND ao='BOKwiEdAMzQ'
 
+-- gets data for enteredData.test.js
+SELECT *
+FROM analytics
+WHERE pe='2019Q3'
+AND dx='Q7dzwYIVj9C'
+AND (co='QPO0ZfoGC5D' OR co='l6gFAecb5ua')
