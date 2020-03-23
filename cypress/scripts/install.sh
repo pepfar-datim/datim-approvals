648ce279835a889b3c76e7bbf9598162423ec157
Jakub <jflaska@baosystems.com>
2020-03-23 14:57:21 -0400

    fixing test issues

---
+++ cypress/scripts/install.sh
@@ -0,0 +1,3 @@
+python dataStore/fixDataStore.py
+python mechanism/submitMechanisms.py
+(cd users; python persist_users.py)
--- cypress/scripts/mechanism/submitMechanisms.py
+++ cypress/scripts/mechanism/submitMechanisms.py
@@ -1,9 +1,13 @@
 import api
 
+
+# ER Expenditures FYOct, 2018Oct, Asia Region, 17286 - 6NU2GGH001346 - Care, Support and Treatment - HIV/TB Project, submitted by agency
+erGlobal = {"approvals":[{"aoc":"m0NndYanyMR","ou":"ybg3MO3hcf4"}],"pe":["2018Oct"],"wf":["WUD8TApgOu1"]}
 er = {"approvals":[{"aoc":"m0NndYanyMR","ou":"ptVxnBssua6"}],"pe":["2018Oct"],"wf":["WUD8TApgOu1"]}
 target = {"approvals":[{"aoc":"ftafnQZ4uml","ou":"YM6xn5QxNpY"}],"pe":["2019Oct"],"wf":["TAjCBkG6hl6"]}
 
 # return to pending at partner
+api.post('dataApprovals/unapprovals', erGlobal)
 api.post('dataApprovals/unapprovals', er)
 api.post('dataApprovals/unapprovals', er)
 api.post('dataApprovals/unapprovals', er)
