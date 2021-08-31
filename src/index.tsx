1cb2f47611c9c433f97022732d0b6d0867ed23f9
Jakub <jflaska@baosystems.com>
2021-08-31 15:29:28 -0400

    new index

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import {render} from 'react-dom';
 import {baseUrl} from "./modules/shared/services/apiUrl.service";
-import {config, init} from 'd2';
-import {HeaderBar} from '@dhis2/ui-widgets'
-import {Provider} from '@dhis2/app-runtime'
-
+import {Provider} from "@dhis2/app-runtime";
+import {HeaderBar} from '@dhis2/ui'
 import ThemeWrapper from "./modules/main/components/themeWrapper.component";
-import NetworkError from "./modules/main/components/networkError.component";
 import "./index.css";
 
-config.baseUrl = baseUrl + 'api';
-config.i18n.sources.add('i18n.txt');
-
-function Dhis2(){
+function Index(){
     return (
         <Provider config={{baseUrl: baseUrl, apiVersion: 33}}>
             <span id='dhis2HeaderBar'>
@@ -23,9 +17,4 @@ function Dhis2(){
         </Provider>
     );
 }
-
-init().then(d2 => {
-    render(<Dhis2/>, document.getElementById('root'));
-}).catch(e=>{
-    render(<NetworkError/>, document.getElementById('root'));
-});
+render(<Index/>, document.getElementById('root'));
