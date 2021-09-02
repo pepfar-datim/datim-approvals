d9ba31057825836c5f2a2156176994c907476d3a
Jakub <jflaska@baosystems.com>
2021-09-02 13:38:28 -0400

    testing works

--- src/test/search/searchDedupes/1.searchDedupes.test.tsx
+++ src/test/search/searchDedupes/1.searchDedupes.test.tsx
@@ -1,12 +1,26 @@
 import {renderSearch} from "../../shared/setup.testServices";
 import Filters from "../../../modules/list/models/filters.model";
-import {texts} from "@pepfar-react-lib/jest-tools";
+import {click, noTexts, texts} from "@pepfar-react-lib/jest-tools";
+
+type TestAction = {
+    click?: string;
+    texts?: string[];
+    noTexts?: string[]
+}
 
 type TestCase = {
     name: string;
     filters: Filters;
-    expected: string[];
+    actions: TestAction[];
 }
+
+const botswanaViewPage = [
+    '7320 - StateAFLaboratory - RPSO laboratory construction projects',
+    '9915 - GH001116 - Capacity building assistance for global HIV/AIDS microbiological labs',
+    'American Society For Microbiology',
+    '17275 - GGH001314 - Voluntary Medical Male Circumcision',
+    'accepted by global'
+];
 
 const testCases:TestCase[] = [{
     name: 'MER Results > Oct - Dec 2020 > Botswana',
@@ -15,19 +29,25 @@ const testCases:TestCase[] = [{
         period: 'Oct - Dec 2020',
         ou: 'Botswana',
     },
-    expected: [
-        '7320 - StateAFLaboratory - RPSO laboratory construction projects',
-        '9915 - GH001116 - Capacity building assistance for global HIV/AIDS microbiological labs',
-        'American Society For Microbiology',
-        '17275 - GGH001314 - Voluntary Medical Male Circumcision',
-        'accepted by global'
-    ]
+    actions: [{
+        texts: botswanaViewPage
+    },{
+        click: 'resultTabs_submit',
+        noTexts: botswanaViewPage
+    }]
 }]
+
+
+function performTestAction(action:TestAction){
+    if (action.click) click(action.click);
+    if (action.texts) texts(action.texts);
+    if (action.noTexts) noTexts(action.noTexts);
+}
 
 function searchDedupesTest(testCase:TestCase){
     test(`1 > Search Dedupes > ${testCase.name}`, async()=>{
         await renderSearch(testCase.filters);
-        texts(textToFind);
+        testCase.actions.forEach(performTestAction);
     })
 }
 
